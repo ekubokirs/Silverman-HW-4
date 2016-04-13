08431317f561c1017082ca870f4055e5eda57622
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2016-04-12 20:11:43 -0400

    added comment

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
+// Tabbing in will make the code easier to read and debug.  The last curly brace is always the one that messes me up
+	function learnMore(event) {
 
-function learnMore(event) {
+		event.preventDefault();
+		$('.learnmore').hide();
+		$('#learnmoretext').slideDown();
+	}
 
-	event.preventDefault();
-	$('.learnmore').hide();
-	$('#learnmoretext').slideDown();
-}
-
-$('.learnmore').click(learnMore);
-
-
-function readMore() {
-	
-	event.preventDefault();
-	$('#show-this-on-click').slideDown();
-	$('.readless').show();
-	$('.readmore').hide();
-}
-
-$('.readmore').click(readMore);
+	$('.learnmore').click(learnMore);
 
 
-function readLess() {
+	function readMore() {
+		
+		event.preventDefault();
+		$('#show-this-on-click').slideDown();
+		$('.readless').show();
+		$('.readmore').hide();
+	}
 
-	event.preventDefault();
-	$('#show-this-on-click').slideUp();
-	$('.readless').hide();
-	$('.readmore').show();
-}
+	$('.readmore').click(readMore);
 
-$('.readless').click(readLess);
-     // $('#learnmoretext').hide();
-    	// $('#show-this-on-click').slideDown();
-    	
+
+	function readLess() {
+
+		event.preventDefault();
+		$('#show-this-on-click').slideUp();
+		$('.readless').hide();
+		$('.readmore').show();
+	}
+
+	$('.readless').click(readLess);
+	     // $('#learnmoretext').hide();
+	    	// $('#show-this-on-click').slideDown();
+	    	
 
 }); 
